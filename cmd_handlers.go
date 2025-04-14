8506bfdf3e77ba49a2ce0fcdda37b8d8e907cced
thepralad <prgurung04@gmail.com>
2025-04-14 23:19:44 +0530

    added register cmd

--- cmd_handlers.go
+++ cmd_handlers.go
@@ -1,20 +1,58 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"gator/internal/database"
+	"math/rand"
+	"os"
+	"time"
 )
 
-//Login Handler
-func handlerLogin(s *state, cmd command) error{
-	if len(cmd.Args) < 1{
+// Login Handler
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.Args) < 1 {
 		return fmt.Errorf("The args is empty :(")
 	}
-	
+
 	err := s.cfg.SetUser(cmd.Args[0])
-	if err != nil{
+	if err != nil {
 		return fmt.Errorf("Not being able to switch user :(")
 	}
 
 	fmt.Println("User Switched Successfully")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) < 1 {
+		return fmt.Errorf("The args is empty :(")
+	}
+
+	name := sql.NullString{
+		String: cmd.Args[0],
+		Valid:  true,
+	}
+	id := int32(rand.Intn(100))
+	args := database.CreateUserParams{
+		ID:        id,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+	}
+
+	ctx := context.Background()
+	ctxWithTimeout, _ := context.WithTimeout(ctx, 3*time.Second)
+
+	_, err := s.db.GetUser(ctxWithTimeout, name)
+	if err == nil {
+		os.Exit(1)
+	}
+	user, err := s.db.CreateUser(ctxWithTimeout, args)
+	_ = user
+	if err != nil {
+		return fmt.Errorf("not being able to switch user :(")
+	}
 	return nil
 }
